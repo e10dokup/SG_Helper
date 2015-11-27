91eed25cc8bb4c5795c7d82967ba2c35b4d98fa8
Yoshihiro WADA <1365196t@stu.kobe-u.ac.jp>
2015-11-27 17:36:27 +0900

    Revert "Revert "つらい""
    This reverts commit 36f95067e33489ef3893017f73c80606fd276e09.

--- app/src/main/java/info/e10dokup/slidecontroller/core/BaseActivity.java
+++ app/src/main/java/info/e10dokup/slidecontroller/core/BaseActivity.java
@@ -1,4 +1,4 @@
-package info.e10dokup.androidquicktemplate.core;
+package info.e10dokup.slidecontroller.core;
 
 import android.support.v7.app.AppCompatActivity;
 
--- app/src/main/java/info/e10dokup/slidecontroller/core/BaseFragment.java
+++ app/src/main/java/info/e10dokup/slidecontroller/core/BaseFragment.java
@@ -1,4 +1,4 @@
-package info.e10dokup.androidquicktemplate.core;
+package info.e10dokup.slidecontroller.core;
 
 import android.support.v4.app.Fragment;
 
